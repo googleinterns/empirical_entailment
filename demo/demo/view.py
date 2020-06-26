05f5b23769e8282df1a5e1fc72ef7a09e6a96047
Sihao Chen <sihaoc@google.com>
2020-06-26 16:22:44 -0400

    minor

--- demo/demo/view.py
+++ demo/demo/view.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from demo.model.bart_xsum import produce_summary
+from django.views.decorators.csrf import csrf_exempt
 
 
 def render_home_page(request):
@@ -8,6 +9,7 @@ def render_home_page(request):
     return render(request, 'index.html', context)
 
 
+@csrf_exempt
 def api_produce_summary(request):
     if request.method != 'POST':
         return HttpResponse(status=405)
