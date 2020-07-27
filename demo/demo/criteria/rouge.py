45dc25343c00bd8d9d34ec408f12f443d8edc134
schen149 <schen149@illinois.edu>
2020-07-27 11:17:05 -0400

    fix doc for rouge scorer

--- demo/demo/criteria/rouge.py
+++ demo/demo/criteria/rouge.py
@@ -5,15 +5,15 @@ scorer = rouge_scorer.RougeScorer(['rouge1', 'rougeL'], use_stemmer=True)
 def calcuate_rouge_score(summary: str,
                          reference: str) -> dict:
     """
-    Get the Rouge-1 and Rouge-2 score for a (summary, reference) pair
+    Get the Rouge-1 and Rouge-L score for a (summary, reference) pair
 
-    See: https://github.com/danieldeutsch/sacrerouge
+    See: https://pypi.org/project/rouge-score/
 
     :param summary:
     :param reference:
     :return: Example below
-    {'rouge-1': {'recall': 50.0, 'precision': 57.143, 'f1': 53.333},
-    'rouge-2': {'recall': 14.285999999999998, 'precision': 16.667, 'f1': 15.384999999999998}}
+    {'rouge-1': Score(recall=50.0, precision=57.143, fmeasure=53.333),
+    'rouge-L': Score(recall=50.0, precision=57.143, fmeasure=53.333)}
 
     """
     _score = scorer.score(summary, reference)
